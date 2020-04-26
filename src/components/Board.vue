6c0f4da2a71aa3527bc2048c1aa4e16e2c2f205f
shodayuki <yshoda1227@gmail.com>
2020-04-26 22:47:09 +0900

    タスクの数を表示

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -2,7 +2,7 @@
   <div>
     <header>my Trello</header>
     <main>
-      <p class="info-line">All: 0 tasks</p>
+      <p class="info-line">All: {{ totalCardCount }} tasks</p>
       <div class="list-index">
         <list v-for="(item, index) in lists"
               :key="item.id"
@@ -30,6 +30,9 @@ export default {
     ...mapState([
       'lists'
     ]),
+    totalCardCount() {
+      return this.$store.getters.totalCardCount
+    }
   },
 }
 </script>
